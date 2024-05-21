bb5979344b564cbd82db6175d9e50b2b27f74e3b
Josh S. <aaabccc87@protonmail.com>
2024-05-21 12:33:30 -0400

    view and navigate to all sets of cards

---
+++ src/app/cards/page.tsx
@@ -0,0 +1,47 @@
+import { Metadata } from 'next';
+import { QueryResultRow, sql } from '@vercel/postgres';
+import ExpandableCard from '@/app/components/ExpandableCard';
+import { unstable_noStore as noStore } from 'next/cache';
+
+export const metadata: Metadata = {
+  title: 'Browse Cards'
+}
+
+async function getSets() {
+  const result = await sql`
+    SELECT * FROM set;
+  `; 
+
+  return result.rows;
+}
+
+export default async function ListCards(req: Request) {
+  noStore();
+  const sets = await getSets();
+
+  return (
+    <>
+      <h1>Sets</h1>
+      {
+        sets.map((set: QueryResultRow) => {
+          const currentSet: CardSet = {
+            id: set.id,
+            title: set.name,
+            dateCreated: set.datecreated,
+            description: set.description,
+          }
+
+          console.log(currentSet);          
+          return (
+            <div key={currentSet.id}>
+              <p><a href={`/set/${currentSet.id}`}>{currentSet.title}</a><span>{currentSet.dateCreated.toLocaleString()}</span></p>
+              <div>
+                <p>{currentSet.description}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+    </>
+  )
+}
