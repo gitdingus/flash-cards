643c2f3b2a1575aaa628f29db9fdbb39b2d3e4bb
Josh S. <aaabccc87@protonmail.com>
2024-05-30 13:30:26 -0400

    salt toString on creation, hash toString on assignment

--- src/utils/passwords.ts
+++ src/utils/passwords.ts
@@ -1,15 +1,14 @@
 import { randomBytes, pbkdf2Sync } from 'crypto';
 
 export function saltHash(password: string) {
-  const salt = randomBytes(32);
+  const salt = randomBytes(32).toString();
   const pwdNormalized = password.normalize();
-  const hash = pbkdf2Sync(pwdNormalized, salt, 1000, 64, 'sha-512');
+  const hash = pbkdf2Sync(pwdNormalized, salt, 1000, 64, 'sha-512').toString();
 
-  return { salt: salt.toString(), hash: hash.toString() }
+  return { salt, hash }
 }
 
 export function verifyPassword(password: string, salt: string, hash: string) {
   const newHash = pbkdf2Sync(password.normalize(), salt, 1000, 64, 'sha-512').toString();
-
   return hash === newHash;
 }
