c21f8d82609740a9ad173da822d4873d82f16614
Josh S. <aaabccc87@protonmail.com>
2024-06-10 17:00:00 -0400

    getSet function compiles info from db to return SetOfCards

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,6 +1,51 @@
 'use server';
-import { sql } from '@vercel/postgres';
+import { sql, db } from '@vercel/postgres';
 import { auth } from '@/auth';
+
+export async function getSet(id: string) {
+  const client = await db.connect();
+
+  const [ setQuery, cardsQuery ] = await Promise.all([
+    client.sql`SELECT * FROM set WHERE id = ${id};`,
+    client.sql`SELECT * FROM card WHERE inset = ${id};`
+  ]);
+
+  if (setQuery.rows.length === 0) {
+    return null;
+  }
+
+  const setRecord = setQuery.rows[0];
+  const set: SetInfo = {
+    id: setRecord.id,
+    title: setRecord.name,
+    description: setRecord.description,
+    dateCreated: setRecord.datecreated,
+    owner: setRecord.owner,
+    isPublic: setRecord.public,
+  }
+
+  const cards: CardBase[] = await Promise.all(cardsQuery.rows.map(async (cardResult) => {
+    const linesQuery = await client.sql`SELECT * FROM cardline WHERE cardid = ${cardResult.id}`;
+    const lines: Line[] = linesQuery.rows.map((line) => { return { heading: line.heading, content: line.content }});
+    const card: CardBase = {
+      id: cardResult.id,
+      dateCreated: cardResult.datecreated,
+      front: { title: cardResult.title },
+      back: { lines },
+    }
+    
+    return card;
+  }));
+
+  client.release();
+  
+  const setOfCards: SetOfCards = {
+    ...set,
+    cards,
+  }
+
+  return setOfCards;
+}
 
 export async function getSets() {
   const result = await sql`
