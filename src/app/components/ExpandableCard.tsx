5d0b67de963840adf64b14f6629800d7f86e6b3f
Josh S. <aaabccc87@protonmail.com>
2024-06-10 17:02:29 -0400

    takes in CardBase which has all relevant info, remove useEffect to get info on card

--- src/app/components/ExpandableCard.tsx
+++ src/app/components/ExpandableCard.tsx
@@ -3,23 +3,12 @@ import { useEffect, useState } from 'react';
 import { getCardData } from '@/actions/card-actions';
 
 interface Props {
-  card: CardRecord,
+  card: CardBase,
   children?: React.ReactNode,
 }
 
 export default function ExpandableCard({ card, children }: Props) {
   const [ expanded, setExpanded ] = useState(false);
-  const [ lines, setLines ] = useState<LineRecord[]>([]);
-
-  useEffect(() => {
-    const getIt = async () => {
-      const gotLines = await getCardData(card.id);
-      setLines(gotLines);
-    };
-
-    getIt();
-
-  }, [card.id]);
 
   const click = () => {
     setExpanded(!expanded);
@@ -27,13 +16,13 @@ export default function ExpandableCard({ card, children }: Props) {
 
   return (
     <div>
-      <button type="button" onClick={click}>{card.title}</button>
+      <button type="button" onClick={click}>{card.front.title}</button>
       <div className={`card ${expanded ? 'expanded' : ''}`}>
         <ul>
           {
-            lines.map((line) => {
+            card.back.lines.map((line) => {
               return (
-                <li key={`${line.cardId}-${line.heading}`}>
+                <li key={`${card.id}-${line.heading}`}>
                   <p><span>{line.heading}</span>: <span>{line.content}</span></p>
                 </li>
               )
