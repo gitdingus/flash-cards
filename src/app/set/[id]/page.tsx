1f9585e0daeba0ad4677348970403b7bdf28d008
Josh S. <aaabccc87@protonmail.com>
2024-06-10 17:03:10 -0400

    use getSet instead of query from component

--- src/app/set/[id]/page.tsx
+++ src/app/set/[id]/page.tsx
@@ -1,32 +1,25 @@
 import { sql } from '@vercel/postgres';
+import { getSet } from '@/app/lib/data';
 import ExpandableCard from '@/app/components/ExpandableCard';
 
 export default async function SetInfo({ params }: { params: { id: string } }) {
-  const set = await sql`
-    SELECT * FROM set WHERE id = ${params.id};
-  `;
+  const set = await getSet(params.id);
 
-  const cards = await sql`
-    SELECT * FROM card WHERE inset = ${params.id};
-  `;
+  if (!set) {
+    return <div>Not Found</div>
+  }
 
   return (
     <div>
       <h1>Set</h1>
-      <p>{set.rows[0].name}</p>
-      <p>{set.rows[0].description}</p>
+      <p>{set.title}</p>
+      <p>{set.description}</p>
       <ul>
         {
-          cards.rows.map((card) => {
-            const currentCard: CardRecord = {
-              id: card.id,
-              inSet: card.inset,
-              dateCreated: new Date(card.datecreated),
-              title: card.title,
-            }
+          set.cards.map((card) => {
             return(
               <li key={card.id}>
-                <ExpandableCard card={currentCard} />
+                <ExpandableCard card={card} />
               </li>
             )
           })
