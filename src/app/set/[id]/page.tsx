5721c2f93a9263d9c252d57e8ffc8c85f92a5638
Josh S. <aaabccc87@protonmail.com>
2024-06-12 11:11:06 -0400

    Show who created set Add link to edit set if owned by logged in user

--- src/app/set/[id]/page.tsx
+++ src/app/set/[id]/page.tsx
@@ -1,19 +1,34 @@
 import { sql } from '@vercel/postgres';
 import { getSet } from '@/app/lib/data';
+import { getUserById } from '@/app/lib/accounts';
+import { auth } from '@/auth';
 import ExpandableCard from '@/app/components/ExpandableCard';
 
 export default async function SetInfo({ params }: { params: { id: string } }) {
-  const set = await getSet(params.id);
+  const [set, session] = await Promise.all([
+    getSet(params.id),
+    auth(),
+  ])
 
   if (!set) {
     return <div>Not Found</div>
   }
+  
+  const owner = await getUserById(set.owner);
 
   return (
     <div>
       <h1>Set</h1>
       <p>{set.title}</p>
       <p>{set.description}</p>
+      {
+        owner &&
+        <p>Created by: <span>{owner.username}</span></p>
+      }
+      {
+        session?.user.userId === set.owner && 
+        <a href={`/set/${set.id}/edit`}>Edit Set</a>
+      }
       <ul>
         {
           set.cards.map((card) => {
