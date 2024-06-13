7cb16e4d257650b59ed25bbfba6e437beb6f12da
Josh S. <aaabccc87@protonmail.com>
2024-06-13 11:39:41 -0400

    developing route to edit sets

---
+++ src/app/set/[id]/edit/page.tsx
@@ -0,0 +1,51 @@
+import { getSet } from "@/app/lib/data";
+import { auth } from "@/auth";
+import SetInput from "@/app/components/set-tools/SetInput";
+
+interface EditSetParams {
+  params: {
+    id: string,
+  }
+}
+export default async function EditSet({ params }: EditSetParams) {
+  const [set, session] = await Promise.all([
+    getSet(params.id),
+    auth(),
+  ]);
+
+  if (!set) {
+    return <div>Not Found</div>
+  }
+
+  if (!session || session.user.userId !== set.owner) {
+    return <div>Unauthorized</div>
+  }
+
+  return (
+    <SetInput 
+      set={set} 
+      submitAction={async ({set, cardsInSet}, formData) => {
+        'use server';
+        console.log(set);
+        console.log(cardsInSet);
+        console.log('form data keys');
+      }}
+      removeCard={ async (card) => { 
+        'use server';
+        console.log(`removing card ${card.front.title}`); 
+      }}
+      removeLine={ async (line) => {
+        'use server';
+        console.log(`removing line ${line.id}: ${line.heading}`);
+      }}
+      editLine={ async (line) => {
+        'use server';
+        console.log(`editing line ${line.id} to ${line.heading}: ${line.content}`);
+      }}
+      saveLine={ async (line) => {
+        'use server';
+        console.log(`saving line ${line.id} ${line.heading} ${line.content}`);
+      }}
+    />
+  )
+}
