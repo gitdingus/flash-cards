ca8144de31d82f6ce1d74704ba03799a68c0f42e
Josh S. <aaabccc87@protonmail.com>
2024-06-18 10:56:51 -0400

    component manages accessibility of a set

--- src/app/components/set-tools/SetPermissions.tsx
+++ src/app/components/set-tools/SetPermissions.tsx
@@ -1,50 +1,88 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { setVisibility } from '@/actions/set-actions';
+import { getAllowedUsersOfSet } from '@/app/lib/data';
 
 interface SetPermissionsParams {
   set: SetInfoBase,
-  allowedUsers: PublicUser[],
 }
 
-type PermissionsOptions = "public" | "allow" | "revoke";
+type SetVisibility = "public" | "private"
+type PermissionsOptions = "allow" | "revoke" | undefined;
 
-export default function SetPermissions({ set, allowedUsers }: SetPermissionsParams) {
-  const [ permissionOption, setPermissionOption ] = useState<PermissionsOptions>(set.isPublic ? "public" : "allow");
+export default function SetPermissions({ set }: SetPermissionsParams) {
+  const [ isPublic, setIsPublic ] = useState<SetVisibility>(set.isPublic ? "public" : "private");
+  const [ permissionOption, setPermissionOption ] = useState<PermissionsOptions>("allow");
+  const [ allowedUsers, setAllowedUsers ] = useState<PublicUser[]>([]);
+
+  useEffect(() => {
+    if (permissionOption === "revoke") {
+      getAllowedUsersOfSet(set.id)
+        .then((users) => {
+          console.log(users);
+          setAllowedUsers(users);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [permissionOption, set.id]);
+
   return (
     <div>
-      <form>
+      <form action={setVisibility}>
+        <input type="hidden" name="setId" value={set.id} />
         <select
-          name="permission-action"
+          name="visibility"
           onChange={
             (e) => {
               switch (e.target.value) {
-                case "allow":
-                  setPermissionOption("allow");
-                  break;
-                case "revoke":
-                  setPermissionOption("revoke");
-                  break;
                 case "public":
-                  setPermissionOption("public");
+                  setIsPublic("public");
+                  break;
+                case "private":
+                  setIsPublic("private");
+                  break;
               }
           }}
-          defaultValue={permissionOption}
+          defaultValue={isPublic}
         >
-          <option value="allow">Allow</option>
-          <option value="revoke">Revoke</option>
           <option value="public">Public</option>
+          <option value="private">Private</option>
         </select>
         {
-          permissionOption === "allow" &&
+          isPublic === "private" &&
+          <select
+            name="permissions"
+            onChange={
+              (e) => {
+                switch (e.target.value) {
+                  case "allow":
+                    setPermissionOption("allow");
+                    break;
+                  case "revoke":
+                    setPermissionOption("revoke");
+                    break;
+                }
+              }
+            }
+            defaultValue={permissionOption}
+          >
+            <option value="allow">Allow</option>
+            <option value="revoke">Revoke</option>
+          </select>
+        }
+        {
+          isPublic === "private" && permissionOption === "allow" && 
           <input type="text" name="user" />
         }
         {
-          permissionOption === "revoke" &&
+          isPublic === "private" && permissionOption === "revoke" && 
           <select name="user" defaultValue="null">
             <option disabled value="null">Select user</option>
             {
               allowedUsers.map((user) => (
-                <option key={user.id} value={user.username}>{user.username}</option>
+                <option key={user.id} value={user.id}>{user.username}</option>
               ))
             }
           </select>
