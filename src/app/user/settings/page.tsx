856902d4bb007a5190e0caa7117e802916897475
Josh S. <aaabccc87@protonmail.com>
2024-06-22 00:14:48 -0400

    use ChangePasswordForm, skeleton for change email

--- src/app/user/settings/page.tsx
+++ src/app/user/settings/page.tsx
@@ -1,5 +1,23 @@
-export default function UserSettings() {
+import { auth } from '@/auth';
+import ChangePasswordForm from "@/app/components/account-tools/ChangePasswordForm";
+
+export default async function UserSettings() { 
+  const session = await auth();
+
+  if (!session) {
+    return <div>Unauthorized</div>
+  }
+  
   return (
-    <div>Hello settings!</div>
+    <div>
+      <ChangePasswordForm />
+      <form>
+        <label>
+          New Email 
+          <input type="email" name="new-email" />
+        </label>
+        <button type="submit">Change Email</button>
+      </form>
+    </div>
   )
 }
