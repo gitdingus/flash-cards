e4cc6611d2f6bfd7c9dc7610244e17a330ec7b10
Josh S. <aaabccc87@protonmail.com>
2024-06-22 00:15:27 -0400

    fix bug: use toString(hex) in saltHash and verifyPassword

--- src/utils/passwords.ts
+++ src/utils/passwords.ts
@@ -1,14 +1,15 @@
 import { randomBytes, pbkdf2Sync } from 'crypto';
 
 export function saltHash(password: string) {
-  const salt = randomBytes(32).toString();
-  const pwdNormalized = password.normalize();
-  const hash = pbkdf2Sync(pwdNormalized, salt, 1000, 64, 'sha-512').toString();
+  const normalizedPwd = password.normalize();
+  const salt = randomBytes(32).toString('hex');
+  const hash = pbkdf2Sync(normalizedPwd, salt, 1000, 64, 'sha-512').toString('hex');
 
   return { salt, hash }
 }
 
 export function verifyPassword(password: string, salt: string, hash: string) {
-  const newHash = pbkdf2Sync(password.normalize(), salt, 1000, 64, 'sha-512').toString();
+  const normalizedPwd = password.normalize();
+  const newHash = pbkdf2Sync(normalizedPwd, salt, 1000, 64, 'sha-512').toString('hex');
   return hash === newHash;
 }
