a2a4ea03f2ea2c8b2222dbdb4cc795ea2d904ffd
Josh S. <aaabccc87@protonmail.com>
2024-06-25 21:39:31 -0400

    hasAccessToSet takes ownerId, logged in userId and setId returns boolean if allowed to view a set

---
+++ src/app/lib/permissions.ts
@@ -0,0 +1,14 @@
+import { sql } from '@vercel/postgres';
+
+export async function hasAccessToSet(ownerId: string, userId: string, setId: string) {
+  const setPermissionQuery = await sql`
+    SELECT * 
+    FROM setpermission
+    WHERE 
+      userid = ${userId}
+      AND setid = ${setId}
+  ;`;
+
+  return ownerId === userId 
+    || (setPermissionQuery.rowCount > 0 && setPermissionQuery.rows[0].granted === true);
+}
