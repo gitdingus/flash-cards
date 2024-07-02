b4e0cc21aaadac323ebbe2a180dfb49deeaf493e
Josh S. <aaabccc87@protonmail.com>
2024-07-02 10:54:08 -0400

    update use of LinkMarkdownTransformer

--- src/app/components/notification-tools/Notification.tsx
+++ src/app/components/notification-tools/Notification.tsx
@@ -1,13 +1,15 @@
 import LinkMarkDownTransformer from "@/app/components/markdown-components/LinkMarkDownTransformer"
-
+import { headers } from "next/headers";
 interface NotificationProps {
   notification: NotificationBase,
 } 
 export default function Notification({ notification }: NotificationProps) {
+  const host = headers().get('host');
+
   return (
     <div>
       <p>{notification.subject}</p>
-      <LinkMarkDownTransformer text={notification.content} />
+      <LinkMarkDownTransformer text={notification.content} baseUrl={host} />
     </div>
   )
 }
