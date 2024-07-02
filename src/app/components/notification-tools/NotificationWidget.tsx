4b2f40d212780e568cb235a199418cff34b0eec3
Josh S. <aaabccc87@protonmail.com>
2024-07-02 10:54:43 -0400

    turn into client component in order to make interactive

--- src/app/components/notification-tools/NotificationWidget.tsx
+++ src/app/components/notification-tools/NotificationWidget.tsx
@@ -1,30 +1,47 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import { Session } from "next-auth"
 import { getNotifications } from "@/app/lib/notifications"
 import LinkMarkDownTransformer from "@/app/components/markdown-components/LinkMarkDownTransformer";
 
-export default async function NotificationWidget({ session }: { session: Session }) {
-  const { notifications, hasMore } = await getNotifications(session.user.userId, { limit: 5, viewed: false });
+interface NotificationWidgetConfig {
+  session: Session,
+}
 
-  if (notifications.length === 0) {
+export default function NotificationWidget({ session }: NotificationWidgetConfig) {
+  const [currentNotifications, setCurrentNotifications] = useState<NotificationBase[]>([]);
+  const [moreNotifications, setMoreNotifications] = useState(false);
+
+  useEffect(() => {
+    getNotifications(session.user.userId, { limit: 5, viewed: false })
+      .then((data) => {
+        const { notifications, hasMore } = data;
+        setCurrentNotifications(notifications);
+        setMoreNotifications(hasMore);
+      });
+  }, [session.user.userId]);
+
+  if (currentNotifications.length === 0) {
     return <div>No new notifications</div>
   } else {
     return (
       <div>
-        <p>You have {notifications.length} unread notification(s)</p>
+        <p>You have {currentNotifications.length} unread notification(s)</p>
         <ul>
           {
-            notifications.map((notification) => {
+            currentNotifications.map((notification) => {
               return (
-                <li key={notification.id}>
+                <li key={notification.id} onClick={(e) => { console.log('clicked muthafucka')}}>
                   <div>
-                    <LinkMarkDownTransformer text={notification.content} />
+                    <LinkMarkDownTransformer baseUrl={window.location.host} text={notification.content} />
                   </div>
                 </li>
               )
             })
           }
         </ul>
-        <p><a href='/user/notifications'>{`View ${hasMore ? 'more' : 'all'} notifications`}</a></p>
+        <p><a href='/user/notifications'>{`View ${moreNotifications ? 'more' : 'all'} notifications`}</a></p>
       </div>
     )
   }
