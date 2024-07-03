a9fbffb42ebb822f7353cc4b35da413ebb2a1b1b
Josh S. <aaabccc87@protonmail.com>
2024-07-03 12:13:34 -0400

    convert to client component, display datecreated

--- src/app/components/notification-tools/Notification.tsx
+++ src/app/components/notification-tools/Notification.tsx
@@ -1,14 +1,15 @@
+'use client';
 import LinkMarkDownTransformer from "@/app/components/markdown-components/LinkMarkDownTransformer"
 import { headers } from "next/headers";
 interface NotificationProps {
   notification: NotificationBase,
 } 
 export default function Notification({ notification }: NotificationProps) {
-  const host = headers().get('host');
+  const host = window.location.host;
 
   return (
-    <div>
-      <p>{notification.subject}</p>
+    <div className={!notification.viewed ? 'unread' : ''}>
+      <p><span>{notification.subject}</span> <span>{notification.dateCreated.toLocaleString()}</span></p>
       <LinkMarkDownTransformer text={notification.content} baseUrl={host} />
     </div>
   )
