1f51ac4aff3a6776ba74ba92bae3cd3df663d207
Josh S. <aaabccc87@protonmail.com>
2024-07-04 17:33:18 -0400

    add report types

--- src/types/report.d.ts
+++ src/types/report.d.ts
@@ -5,11 +5,24 @@ interface ReportBase {
   setId: string,
   reason: string,
   dateCreated: Date,
+  resolved: boolean,
 } 
 
-interface ResolvedReport extends ReportBase{
-  resolved: boolean,
+interface ResolvedReport extends ReportBase {
   dateResolved: Date,
   moderatedBy: string,
   actionTaken: string,
 }
+
+interface PopulatedReportBase extends ReportBase {
+  reporterName: string,
+  reporteeName: string,
+  setTitle: string,
+}
+
+interface PopulatedResolvedReport extends ResolvedReport, PopulatedReportBase {
+  moderatorName: string,
+}
+
+export type Report = ReportBase | ResolvedReport;
+export type PopulatedReport = PopulatedReportBase | PopulatedResolvedReport;
