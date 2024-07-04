1efd3f562b0543865f27665c0e8a4fed131887ca
Josh S. <aaabccc87@protonmail.com>
2024-07-04 17:42:11 -0400

    create isAdmin function to aid hasAccessToSet and give admins full access

--- src/app/lib/permissions.ts
+++ src/app/lib/permissions.ts
@@ -1,4 +1,5 @@
 'use server';
+import { auth } from '@/auth';
 import { sql } from '@vercel/postgres';
 
 export async function hasAccessToSet(ownerId: string, userId: string, setId: string) {
@@ -10,6 +11,39 @@ export async function hasAccessToSet(ownerId: string, userId: string, setId: str
       AND setid = ${setId}
   ;`;
 
-  return ownerId === userId 
-    || (setPermissionQuery.rowCount > 0 && setPermissionQuery.rows[0].granted === true);
+  if (await isAdmin()) {
+    return true;
+  }
+
+  if (ownerId === userId) {
+    return true;
+  }
+
+  if (setPermissionQuery.rowCount > 0 && setPermissionQuery.rows[0].granted === true) {
+    return true;
+  }
+  
+  return false;
+}
+
+export async function isAdmin() {
+  const session = await auth();
+
+  if (!session) {
+    return false;
+  }
+
+  const isAdminQuery = await sql`
+    SELECT *
+    FROM user_role
+    JOIN role ON user_role.role_id = role.role_id
+    WHERE user_role.user_id = ${session.user.userId}
+      AND role.role = 'admin'
+  ;`;
+
+  if (isAdminQuery.rowCount === 1) {
+    return true;
+  }
+
+  return false;
 }
