faffe1bbe0a304bfb0b014c5206288d95a7597b5
Josh S. <aaabccc87@protonmail.com>
2024-07-25 17:44:23 -0400

    add suspend user option and duration field

--- src/app/reports/set/[id]/ModActionsForm.tsx
+++ src/app/reports/set/[id]/ModActionsForm.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { ChangeEvent, useState } from 'react';
 import { takeModAction } from '@/actions/mod-actions';
 
 interface ModActionsFormProps {
@@ -5,23 +7,43 @@ interface ModActionsFormProps {
 }
 
 export default function ModActionsForm({ id }: ModActionsFormProps) {
+  const [showDurationPicker, setShowDurationPicker] = useState(false);
+
+  function modActionChanged(e: ChangeEvent<HTMLSelectElement>) {
+    if (e.target.value === 'suspend-user'){
+      setShowDurationPicker(true);
+    } else {
+      setShowDurationPicker(false);
+    }
+  }
+
   return (
     <div>
       <form action={takeModAction}>
         <input type="hidden" name="set-id" value={id} />
         <label>
           Mod actions:
-          <select name="mod-action">
+          <select name="mod-action" onChange={modActionChanged}>
             <option value="hide-content">Hide content</option>
             <option value="unhide-content">Unhide content</option>
             <option value="close-no-action">Close - No action</option>
             <option value="remove-content">Remove Content - PERMANENT ACTION</option>
+            <option value="suspend-user">Suspend User</option>
           </select>
         </label>
         <label>
           Explanation:
           <textarea name="explanation"></textarea>
         </label>
+        {
+          showDurationPicker && 
+          <select name="duration">
+            <option value="three-days">Three days</option>
+            <option value="one-week">One week</option>
+            <option value="one-month">One month</option>
+            <option value="permanent">Permanent Suspension</option>
+          </select>
+        }
         <button type="submit">Submit</button>
       </form>
     </div>
