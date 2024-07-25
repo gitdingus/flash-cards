1202a3c9ba219897e6fe8c6a818159ceae468af1
Josh S. <aaabccc87@protonmail.com>
2024-07-25 17:45:52 -0400

    isSuspended function

--- src/app/lib/permissions.ts
+++ src/app/lib/permissions.ts
@@ -47,3 +47,44 @@ export async function isAdmin() {
 
   return false;
 }
+
+export async function isSuspended() {
+  const session = await auth();
+
+  if (!session) {
+    // if not logged in just return true to prevent possible api access
+    return true; 
+  }
+
+  const suspendedQuery = await sql`
+    SELECT id, user_id, forever, (active_until AT TIME ZONE 'UTC') AS active_until
+    FROM suspended_user 
+    WHERE user_id = ${session.user.userId}
+    ORDER BY active_until DESC
+    LIMIT 1
+  ;`;
+
+  if (suspendedQuery.rowCount === 0) {
+    return false;
+  }
+
+  const suspension = suspendedQuery.rows[0];
+
+  if (suspension.forever) {
+    return true;
+  }
+
+  if (!suspension.active_until) {
+    // if suspension.forever is false and there is no active_until date
+    // consider them not suspended, this should not happen. 
+    return false; 
+  }
+
+  const now = new Date();
+
+  if (now.valueOf() < suspension.active_until.valueOf()) {
+    return true;
+  }
+
+  return false;
+}
