163512f90e27260668a69be87f113f7189a80d7b
Josh S. <aaabccc87@protonmail.com>
2024-08-12 12:23:09 -0400

    move to new folder

---
+++ src/app/components/set-display/ExpandableCard.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { useEffect, useState } from 'react';
+import { getCardData } from '@/actions/set-actions';
+import styles from '@/app/styles/set/card/expandable-card.module.css';
+
+interface Props {
+  card: CardBase,
+  hideTitleOnFlip?: boolean,
+  children?: React.ReactNode,
+}
+
+type ActiveSide = "front" | "back";
+export default function ExpandableCard({ card, hideTitleOnFlip = false, children }: Props) {
+  const [activeSide, setActiveSide] = useState<ActiveSide>("front");
+
+  const click = () => {
+    if (activeSide === 'front') {
+      setActiveSide('back');
+    } else if (activeSide === 'back') {
+      setActiveSide('front');
+    }
+  }
+
+  const titleClassName = () => {
+    if (activeSide === 'front') {
+      return styles.active;
+    } else if (activeSide === 'back' && !hideTitleOnFlip) {
+      return styles.active;
+    }
+
+    return '';
+  }
+  return (
+    <div onClick={click} className={styles.expandableCard}>
+      <div className={`${titleClassName()}`}>{card.front.title}</div>
+      <div className={`${activeSide === 'back' ? styles.active : ''}`}>
+        <ul>
+          {
+            card.back.lines.map((line) => {
+              return (
+                <li key={`${card.id}-${line.heading}`}>
+                  <p><span>{line.heading}</span>: <span>{line.content}</span></p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </div>
+  )
+}
