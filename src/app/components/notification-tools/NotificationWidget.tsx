58b3a7272db980604c63710d85dae4c9ea548995
Josh S. <aaabccc87@protonmail.com>
2024-08-16 11:33:03 -0400

    Use icons for notification widget and make popup menu

--- src/app/components/notification-tools/NotificationWidget.tsx
+++ src/app/components/notification-tools/NotificationWidget.tsx
@@ -1,11 +1,15 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useRouter } from 'next/navigation';
 import { Session } from "next-auth";
 import { getNotifications } from "@/app/lib/notifications";
 import { markNotificationAsRead } from '@/actions/notification-actions';
+import Image from 'next/image';
 import LinkMarkDownTransformer from "@/app/components/markdown-components/LinkMarkDownTransformer";
+import plainBell from '@/app/images/bell-outline.svg';
+import alertBell from '@/app/images/bell-badge-outline.svg';
+import styles from '@/app/styles/notification-widget/notification-widget.module.css';
 
 interface NotificationWidgetConfig {
   session: Session,
@@ -14,6 +18,8 @@ interface NotificationWidgetConfig {
 export default function NotificationWidget({ session }: NotificationWidgetConfig) {
   const [currentNotifications, setCurrentNotifications] = useState<NotificationBase[]>([]);
   const [moreNotifications, setMoreNotifications] = useState(false);
+  const [showNotificationList, setShowNotificationList] = useState(false);
+  const notificationList = useRef<HTMLUListElement>(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -25,22 +31,75 @@ export default function NotificationWidget({ session }: NotificationWidgetConfig
       });
   }, [session.user.userId]);
 
-  if (!moreNotifications && !currentNotifications.some((notification) => notification.viewed === false)) {
-    return (
-      <div>
-        No new notifications
-        <p><a href='/user/notifications'>View all notifications</a></p>
+  useEffect(() => {
+    function hideNotificationList(e: MouseEvent) {
+      if (notificationList.current 
+        && showNotificationList
+        && e.target !== notificationList.current
+        && e.target instanceof Node
+        && !notificationList.current.contains(e.target)) {
+          setShowNotificationList(false);
+      }
+    }
 
-      </div>
-    )
-  } else {
-    return (
+    if (showNotificationList) {
+      document.addEventListener('click', hideNotificationList);
+    }
+
+    return () => {
+      document.removeEventListener('click', hideNotificationList);
+    }
+  }, [showNotificationList]);
+
+  function unreadNotifications() {
+    if (
+      moreNotifications 
+      || currentNotifications.some((notification) => notification.viewed === false)
+    ){
+      return true;
+    }
+
+    return false;
+  }
+
+  function getIcon() {
+    if (unreadNotifications()) {
+      return (
+        <Image 
+          src={alertBell} 
+          height={24} 
+          width={24} 
+          alt='there are new notifications' 
+          title='There are unread notifications' 
+          onClick={() => {
+            setShowNotificationList(!showNotificationList);
+          }}
+        />
+      )
+    } else {
+      return (
+        <Image 
+          src={plainBell} 
+          height={24} 
+          width={24} 
+          alt='no new notifications' 
+          title='No new notifications' 
+          onClick={() => {
+            setShowNotificationList(!showNotificationList);
+          }}
+        />
+      )
+    }
+  }
+
+  return (
+    <div className={styles.notificationWidget}>
       <div>
-        { 
-          (moreNotifications === true || currentNotifications.some((notification) => notification.viewed === false))
-          && <p>You have unread notification(s)</p>
+        {
+          getIcon()
         }
-        <ul>
+      </div>
+      <ul ref={notificationList} className={`${styles.notificationList} ${showNotificationList ? styles.expanded : ''}`}>
           {
             currentNotifications.map((notification) => {
               return (
@@ -74,10 +133,8 @@ export default function NotificationWidget({ session }: NotificationWidgetConfig
               )
             })
           }
+          <li><a href='/user/notifications'>{`View ${moreNotifications ? 'more' : 'all'} notifications`}</a></li>
         </ul>
-        <p><a href='/user/notifications'>{`View ${moreNotifications ? 'more' : 'all'} notifications`}</a></p>
-      </div>
-    )
-  }
-
+    </div>
+  )
 }
