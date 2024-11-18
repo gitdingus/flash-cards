6d9b47a46709df4d2cb313f4264ffdff12110f74
Josh S. <aaabccc87@protonmail.com>
2024-11-18 15:19:08 -0500

    cards route shows public, private, and owned sets in own galleries removed unneccessary component SetList renamed set-list.module.css to cards-gallery.module.css layout and styling on cards route

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,17 +1,48 @@
 import { Metadata } from 'next';
-import SetList from './SetList';
-import { getAllPublicSets } from '@/app/lib/data';
+import { getAllPublicSets, getAllowedPrivateSets, getOwnSets } from '@/app/lib/data';
+import styles from '@/app/styles/cards/cards-gallery.module.css';
 
 export const metadata: Metadata = {
   title: 'Browse Cards'
 }
 
 export default async function ListCards() {
-  const sets: SetInfo[] = await getAllPublicSets();
+  const [publicSets, privateSets, ownSets ] = await Promise.all([
+    getAllPublicSets(),
+    getAllowedPrivateSets(),
+    getOwnSets(),
+  ]);
 
   return (
-    <>
-      <SetList initialSets={sets} />
-    </>
+    <div className={styles.container}>
+      <SetGallery title="Public Sets" sets={publicSets} more={{ link: '/cards/public', title: "More public sets" }} />
+      <SetGallery title="Private Sets" sets={privateSets} more={{ link: '/cards/private', title: "More private sets" }} />
+      <SetGallery title="My Sets" sets={ownSets} more={{ link: '/cards/mine', title: 'More of my sets' }} />
+    </div>
+  )
+}
+
+function SetGallery({title, sets, more}: { title: string, sets: SetInfo[], more: { link: string, title: string }}) {
+  return (
+    <div className={styles.setGallery}>
+      <h2>{title}</h2>
+      <div className={styles.setList}>
+        {
+          sets.map(set => <SetThumb key={set.id} set={set} />)
+        }
+      </div>
+      <a href={more.link}>{more.title}</a>
+    </div>
+  )
+}
+
+function SetThumb({ set }: { set: SetInfo }) {
+  return (
+    <div className={`${styles.setThumb}`}>
+      <h2 className={styles.title}><a href={`/set/${set.id}`}>{set.title}</a></h2>
+      <p className={styles.description}>{set.description}</p>
+      <p className={styles.link}><a href={`/user/${set.ownerUsername}`}>{set.ownerUsername}</a></p>
+      <p className={styles.cardCount}>{set.cardCount} card(s)</p>
+    </div>
   )
 }
