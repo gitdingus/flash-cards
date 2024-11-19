d42d2c68dbdc49455faaa04270d27107a1ff8e96
Josh S. <aaabccc87@protonmail.com>
2024-11-19 01:33:04 -0500

    get sets on screen with pagination

--- src/app/cards/[...slug]/page.tsx
+++ src/app/cards/[...slug]/page.tsx
@@ -1,18 +1,26 @@
-import { getAllPublicSets, getAllowedPrivateSets, getOwnSets } from '@/app/lib/data';
+import { getAllPublicSets, getAllowedPrivateSets, getOwnSets, SetInfoQueryConfig } from '@/app/lib/data';
+import Link from 'next/link';
 
 interface Props {
   params: {
-    slug: string[],
-    searchParams: {
-      [index: string]: string,
-    },
-  }
+    slug: string[]
+  },
+  searchParams: {
+    [index: string]: string,
+  } 
 }
-export default async function Sets({ params: { slug, searchParams }}: Props) {
-  let queryFn;
+export default async function Sets({ params: { slug }, searchParams}: Props) {
+  let queryFn: (params?: SetInfoQueryConfig) => Promise<SetInfo[]>;
+  const queryOptions: SetInfoQueryConfig = {};
+  const QUERY_LIMIT = 10;
+  queryOptions.limit = QUERY_LIMIT;
 
   if (slug.length !== 1) {
     return <p>Not found</p>
+  }
+
+  if (searchParams.page !== undefined) {
+    queryOptions.offset = Number.parseInt(searchParams.page) * queryOptions.limit;
   }
 
   const path = slug[0];
@@ -30,8 +38,31 @@ export default async function Sets({ params: { slug, searchParams }}: Props) {
       return <p>Not Found</p>
   }
 
-  const sets = await queryFn();
+  console.log(queryOptions);
+  const sets = await queryFn(queryOptions);
 
-  console.log(sets);
-  return <p>Hello Cards</p>
+  return (
+    <div>
+      {
+        sets.map(set => (
+          <div key={set.id}>
+            <h2><Link href={`/set/${set.id}`}>{set.title}</Link></h2>
+            <p>{set.description}</p>
+            <p><Link href={`/user/${set.ownerUsername}`}>{set.ownerUsername}</Link></p>
+            <p>{set.cardCount} card(s)</p>
+          </div>
+        ))
+      }
+      {
+        sets.length >= QUERY_LIMIT
+        &&
+        <Link href={`/cards/${path}?page=${(Number.parseInt(searchParams.page) || 0) + 1}`}>Next</Link>
+      }
+      {
+        (searchParams?.page !== undefined && Number.parseInt(searchParams.page) > 0)
+        &&
+        <Link href={`/cards/${path}?page=${Number.parseInt(searchParams.page) - 1}`}>Prev</Link>
+      }
+    </div>
+  )
 }
