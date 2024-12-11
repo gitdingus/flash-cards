70f193a1efe8dca3b4b868cb64f8c88218615b22
Josh S. <aaabccc87@protonmail.com>
2024-12-11 11:59:50 -0500

    Footer component to display under sets to help set privacy settings and report content

---
+++ src/app/user/[username]/SetFooter.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { useState } from 'react';
+import SetPermissions from '@/app/components/set-tools/SetPermissions';
+import CreateReport from '@/app/components/report/CreateReport';
+import styles from '@/app/styles/user/set-footer.module.css';
+import Expandable from '@/app/components/tools/Expandable';
+type FooterOptions = "none" | "report" | "privacy";
+
+export default function SetFooter({ set, ownSet, loggedIn }: { set: SetInfoBase, ownSet: boolean, loggedIn: boolean }) {
+  const [ footerDisplay, setFooterDisplay ] = useState<FooterOptions>("none");
+
+  function displayOption(option: FooterOptions) {
+    if (option === footerDisplay) {
+      setFooterDisplay("none");
+      return;
+    }
+
+    setFooterDisplay(option);
+
+  }
+  return (
+    <div className={styles.setFooter}>
+      <div className={styles.buttons}>
+        {
+          ownSet &&
+          <button onClick={() => displayOption('privacy')}>Privacy options</button>
+        }
+        {
+          loggedIn && !ownSet &&
+          <button onClick={() => displayOption('report')}>Report</button>
+        }
+      </div>
+      <div className={styles.option}>
+        {
+          footerDisplay === "privacy" &&
+          <SetPermissions set={set} />
+        }
+        {
+          footerDisplay === "report" &&
+          <CreateReport setId={set.id} />
+        }
+      </div>
+    </div>
+  )
+}
