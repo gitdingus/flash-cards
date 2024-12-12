bcca4e7b36bffa9a0a5c2059f3bee8493164cc80
Josh S. <aaabccc87@protonmail.com>
2024-12-12 17:55:19 -0500

    add padding to main content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
           </div>
         </header>
         <AppSessionContext session={session}>
-          {children}
+          <main>{children}</main>
         </AppSessionContext>
       </body>
     </html>
